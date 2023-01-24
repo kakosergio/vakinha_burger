3cae3dd01fadf1d5da7fe7d6800cde13ad77a5f1
Senrathy <sergioricardort@hotmail.com>
2023-01-24 07:09:38 -0300

    Setting up themes

---
+++ lib/app/core/ui/styles/colors_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static ColorsApp? _instance;
+
+  ColorsApp._();
+
+  static ColorsApp get i => _instance ??= ColorsApp._();
+
+  Color get primary => const Color(0XFF007D21);
+  Color get secondary => const Color(0XFFF88B0C);
+}
+
+extension ColorsAppExtensions on BuildContext {
+  ColorsApp get colors => ColorsApp.i;
+}
