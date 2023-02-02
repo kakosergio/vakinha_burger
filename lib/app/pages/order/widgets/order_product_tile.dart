57bd238f6b24752480767e9cfeb5ea2253c9054d
Senrathy <sergioricardort@hotmail.com>
2023-02-02 14:51:45 -0300

    Finished OrderProductTile

--- lib/app/pages/order/widgets/order_product_tile.dart
+++ lib/app/pages/order/widgets/order_product_tile.dart
@@ -1,6 +1,10 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dw9_delivery_app/app/dto/order_product_dto.dart';
+
+import '../../../core/ui/widgets/delivery_increment_decrement_button.dart';
 
 class OrderProductTile extends StatelessWidget {
   final int index;
@@ -14,6 +18,47 @@ class OrderProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('$index');
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Image.network(
+            'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'X-burger',
+                    style: context.textStyles.textRegular.copyWith(fontSize: 16),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        r'R$ 19,90',
+                        style: context.textStyles.textMedium.copyWith(
+                            color: context.colors.secondary, fontSize: 14),
+                      ),
+                      DeliveryIncrementDecrementButton(
+                        amount: 1,
+                        incrementTap: () {},
+                        decrementTap: () {},
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
